cd3f2066bf9269511c5f9fb650d8ac2ce071c1c2
1tanat <sandbreaktimeattack@gmail.com>
2023-10-04 23:12:18 +0300

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,6 +1,8 @@
 package hexlet.code;
+import static hexlet.code.Cli.*;
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to the Brain Games!");
+        Cli.greeting();
     }
 }

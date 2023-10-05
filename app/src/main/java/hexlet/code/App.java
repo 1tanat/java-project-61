2551d023f6276c3d5a3b18a636ae851876fb0a5c
1tanat <sandbreaktimeattack@gmail.com>
2023-10-05 18:02:52 +0300

    update App.java and Cli.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,33 @@
 package hexlet.code;
+
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the game number and press Enter.\n"
+                            + "1 - Greet\n"
+                            + "2 - Even\n"
+                            + "0 - Exit");
+        int choice = scanner.nextInt();
+
+        if (choice == 0) {
+            System.exit(0);
+        }
+
         System.out.println("Welcome to the Brain Games!");
-        Cli.greeting();
+        String name = Cli.greeting();
+
+        switch (choice) {
+            case 1 :
+                break;
+            case 2:
+                Game2.evenOrNot(name);
+                break;
+            default:
+                break;
+        }
+
+        scanner.close();
     }
 }
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -2,12 +2,11 @@ package hexlet.code;
 
 import java.util.Scanner;
 public class Cli {
-    public static void greeting() {
+    public static String greeting() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("May I have your name? ");
         String name = scanner.next();
         System.out.println("Hello, " + name + "!");
-
-        scanner.close();
+        return name;
     }
 }

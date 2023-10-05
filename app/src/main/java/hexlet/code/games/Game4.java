a0c3f9624e9a5ed267f7692fa002b198d10643ab
1tanat <sandbreaktimeattack@gmail.com>
2023-10-05 22:31:30 +0300

    Add Game4.java

---
+++ app/src/main/java/hexlet/code/games/Game4.java
@@ -0,0 +1,24 @@
+package hexlet.code.games;
+
+import org.apache.commons.lang3.RandomUtils;
+
+public class Game4 {
+    public static String[] gcd() {
+        String[] data = new String[2];
+
+        int num1 = RandomUtils.nextInt(1, 100);
+        int num2 = RandomUtils.nextInt(1, 100);
+
+        data[0] = num1 + " " + num2;
+        data[1] = String.valueOf(find_GCD(num1, num2));
+
+        return data;
+    }
+    public static int find_GCD(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return find_GCD(b, a%b);
+        }
+    }
+}

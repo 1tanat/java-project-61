91a3c96a42d934bdf470bb1e7330740d7b2e9e59
1tanat <sandbreaktimeattack@gmail.com>
2023-10-05 21:42:21 +0300

    move Cli.java Game2.java to hexlet/code/games

---
+++ app/src/main/java/hexlet/code/games/Game2.java
@@ -0,0 +1,15 @@
+package hexlet.code;
+
+import org.apache.commons.lang3.RandomUtils;
+import java.util.Scanner;
+public class Game2 {
+    public static String[] even() {
+        String[] data = new String[2];
+
+        int num = RandomUtils.nextInt(1, 100);
+        data[0] = Integer.toString(num);
+        data[1] = (num % 2 == 0) ? "yes" : "no";
+
+        return data;
+    }
+}
